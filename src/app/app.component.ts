a76fbb99b5cc2f50895904420515906c1b6e12b4
sahil <sahiltaneja07@gmail.com>
2017-08-22 00:39:41 +0530

    dynamic table completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { AppService } from './app.service';
 import { Company } from './app.model';
+declare let $: any;
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { Company } from './app.model';
 })
 
 export class AppComponent implements OnInit {
-  columns: string[];
+  columns: {name: string, value: boolean}[];
+  filterColumns: {name: string, value: boolean}[];
   companies: Company[];
 
   constructor(private appService: AppService) {}
@@ -18,17 +20,52 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
   	this.appService.getCompanies()
   		.then((companies: Company[]) => {
-  			console.log(companies);
   			this.companies = companies;
   			this.columns = this.getColumns(this.companies);
+  			this.filterColumns = this.columns.map(x => x);
   		});	
   }
 
-  private getColumns(arr): string[] {
+  private getColumns(arr): {name: string, value: boolean}[] {
   	let columns = Object.keys(arr[0]);
   	columns.splice(columns.indexOf('description'), 1);
   	columns.splice(columns.indexOf('name'), 1);
-  	return columns;
+  	return columns.map(column => {
+  		return {
+  			name: column,
+  			value: true
+  		};
+  	});
+  }
+
+  scrollRight(): void {
+  	$('.table-wrapper').animate({
+    	scrollLeft: 500
+    }, 500);
+  }
+
+  scrollLeft(): void {
+  	$('.table-wrapper').animate({
+    	scrollLeft: 0
+    }, 500);
+  }
+
+  onColumnChange(column): void {
+  	if(column.value){
+  		this.columns.unshift(column);
+  	}
+  	else{
+  		let columnIndex = this.columns.indexOf(column);
+  		this.columns.splice(columnIndex, 1);
+  	}
+  }
+
+  onCustomizeColumnClick(): void {
+  	$('.dropdown').toggle(function() {
+	    $(".dropdown").animate({top: '25px'}, 1000);
+	  }, function() {
+	    $(".dropdown").animate({top: '25px'}, 1000);
+	  });
   }
 
 }
